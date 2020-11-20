53e879747026e983725d4d01a062c332a64ede1d
kaitres <43025286+kaitres@users.noreply.github.com>
2020-11-19 21:08:38 -0300

    Creacion de base de datos
    hecha la base de datos solo falta agregar cosas

--- BaseDatos.sql
+++ BaseDatos.sql
@@ -31,6 +31,49 @@ CREATE TABLE `reunion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Estructura de tabla para la tabla `invitado`
+--
+
+CREATE TABLE `invitado` (
+  `correo` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Estructura de tabla para la tabla `relacionreunioninvitado`
+--
+
+CREATE TABLE `relacionreunioninvitado` (
+  `refcorreo` varchar(50) NOT NULL,
+  `refid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Estructura de tabla para la tabla `tema`
+--
+
+CREATE TABLE `tema` (
+  `tag` varchar(50) NOT NULL,
+  `refreunion` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Estructura de tabla para la tabla `accion`
+--
+
+CREATE TABLE `accion` (
+  `nombre` varchar(50) NOT NULL,
+  `refreunion` int(11) NOT NULL,
+  `reftema` int(11) NOT NULL,
+  `refinvitado` varchar(50) NOT NULL,
+  `fechatermino` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
 -- Volcado de datos para la tabla `reunion`
 --
 
@@ -47,3 +90,57 @@ INSERT INTO `reunion` (`id`, `tipoPredefinido`, `fecha`, `hora`, `duracion`, `ti
 --
 ALTER TABLE `reunion`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `invitado`
+--
+ALTER TABLE `invitado`
+  ADD PRIMARY KEY (`correo`);
+
+--
+-- Indexes for table `relacionreunioninvitado`
+--
+ALTER TABLE `relacionreunioninvitado`
+  ADD KEY `fkrelacioninvitado` (`refcorreo`),
+  ADD KEY `fkrelacionreunion` (`refid`);
+
+--
+-- Indexes for table `tema`
+--
+ALTER TABLE `tema`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkreunion` (`refreunion`);
+
+--
+-- Indexes for table `accion`
+--
+ALTER TABLE `accion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkreuniontema` (`refreunion`),
+  ADD KEY `fkinvitado` (`refinvitado`),
+  ADD KEY `fktema` (`reftema`);
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `relacionreunioninvitado`
+--
+ALTER TABLE `relacionreunioninvitado`
+  ADD CONSTRAINT `fkrelacioninvitado` FOREIGN KEY (`refcorreo`) REFERENCES `invitado` (`correo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fkrelacionreunion` FOREIGN KEY (`refid`) REFERENCES `reunion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tema`
+--
+ALTER TABLE `tema`
+  ADD CONSTRAINT `fkreunion` FOREIGN KEY (`refreunion`) REFERENCES `reunion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `accion`
+--
+ALTER TABLE `accion`
+  ADD CONSTRAINT `fkreuniontema` FOREIGN KEY (`refreunion`) REFERENCES `reunion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fkinvitado` FOREIGN KEY (`refinvitado`) REFERENCES `invitado` (`correo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fktema` FOREIGN KEY (`reftema`) REFERENCES `tema` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
