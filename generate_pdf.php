34ee2a111dddbc23e4178b68c108438c66e30cef
Leo Miranda <leomir2656@gmail.com>
2020-12-02 12:54:02 -0300

    Formato PDF
    agregados los temas, no funciona sin temas la exportacion :/

--- generate_pdf.php
+++ generate_pdf.php
@@ -2,7 +2,7 @@
 
 require_once "vendor/autoload.php";
 
-
+$css = file_get_contents('css/plantillaActa.css');
 
 
 
@@ -10,20 +10,22 @@ require_once "vendor/autoload.php";
 function selectReunion(){
     $v1 = $_POST['variable1'];
     $conexion = mysqli_connect("localhost", "root", "", "gestoractas");
-    $consulta = "SELECT * FROM reunion WHERE id='$v1'";
+    $consulta = "SELECT * FROM reunion, tema WHERE reunion.id='$v1'  AND reunion.id=refreunion";
     $resultado = mysqli_query($conexion, $consulta) or die ( "Algo ha ido mal en la consulta a la base de datos1");
     $tabla="";
    
                 while ($columna = mysqli_fetch_array( $resultado )){
                 $tabla.="
-                        <h1> Comite ICC <h1>
-                        <a>".$columna['id']."</a>
-                        <a>".$columna['tipoPredefinido']."</a>
-                        <a>".$columna['fecha']."</a>
-                        <a>".$columna['hora']."</a>
-                        <a>".$columna['duracion']."</a>
-                        <a>".$columna['tipoDuracion']."</a>
-                        <a>".$columna['linkReunion']."</a>
+                        <h1>Comité Curricular ICC </h1>
+                        <br>
+                        <h2>Reunión ".$columna['tipoPredefinido']."</h2>
+                        <h2>Fecha: ".$columna['fecha']."</h2>
+                        <h2>Hora de Inicio:        ".$columna['hora']."</h2>
+                        <h2>Duracion: ".$columna['duracion']." ".$columna['tipoDuracion']."</h2>
+                        <br>
+                        <h2>Temas Tratados: </h2>
+                        <br>
+                        <h2>".$columna['nombre']."</h2>
                     ";
                    
         } 
@@ -32,6 +34,7 @@ function selectReunion(){
 
 $html = selectReunion();
 $mpdf = new \Mpdf\Mpdf([]);
+$mpdf->WriteHTML($css,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($html);
 $mpdf->Output();
 ?>
