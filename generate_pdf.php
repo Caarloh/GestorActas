41f5c7dbeb15e23b7cdc49ab8ef7d0ec7d026d0b
Leo Miranda <leomir2656@gmail.com>
2020-12-11 11:32:43 -0300

    Identacion PDF
    Formato mas cercano al documento final

--- generate_pdf.php
+++ generate_pdf.php
@@ -13,26 +13,27 @@ function selectReunion(){
     $consulta = "SELECT * FROM reunion WHERE reunion.id='$v1'";
     $resultado = mysqli_query($conexion, $consulta) or die ( "Algo ha ido mal en la consulta a la base de datos1");
     $tabla=""; 
-    $temas="";  
+    $temas="";
+    $finTemas = "</ol>";  
     $consulta2 = "SELECT nombre FROM reunion, tema WHERE reunion.id='$v1' AND reunion.id=refreunion";
     $resultado2 = mysqli_query($conexion, $consulta2) or die ( "Algo ha ido mal en la consulta a la base de datos1");  
         while ($columna2 = mysqli_fetch_array( $resultado2 )){
             $temas.="            
-            <h2>".$columna2['nombre']."</h2>
-            ";                   
+            <li>".$columna2['nombre']."</li>
+            "  ;                
          }
    
                 while ($columna = mysqli_fetch_array( $resultado )){
                 $tabla.="
                 <h1>Comité Curricular ICC </h1>
                 <br>
-                <h2>Reunión ".$columna['tipoPredefinido']."</h2>
+                <h3>Reunión ".$columna['tipoPredefinido']."</h3>
                 <h2>Fecha: ".$columna['fecha']."</h2>
                 <h2>Hora de Inicio:        ".$columna['hora']."</h2>
                 <h2>Duracion: ".$columna['duracion']." ".$columna['tipoDuracion']."</h2>
                 <h2>Temas Tratados:</h2>
-                <br>
-            ".$temas;                   
+                <ol>
+            ".$temas.$finTemas;;                   
         }
     
         
