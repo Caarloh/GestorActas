8a57c625b203705c132bbf5701c7160c15d03c07
Juan <j.pablo.velasc@gmail.com>
2020-12-16 00:43:24 -0300

    Correos bonites para todes el mundes
    funciona el correo de ingreso al sistema, cambio de contrasenia y le puse brillantina al generar PDF

--- baseDatos/agregarInvitadoReunion.php
+++ baseDatos/agregarInvitadoReunion.php
@@ -29,8 +29,7 @@
 
         $consulta = "INSERT INTO invitado (correo, nombre, codigoAcceso) VALUES ('$correo', '$nombre','$contrasenaInvitado')";
         mysqli_query($conexion,$consulta);
-
-        /*Enviar Correo*/
+        //Enviar Correo
         include '../envio_de_correos/superiorenviarcorreo.php';
         //Destinatario
         $mail->addAddress($correo, $nombre);     
@@ -41,7 +40,181 @@
         $topico = "Bienvenido al Gestor De Actas";                                  
         $mail->Subject = $topico;
         
-        $cuerpo = "Hola $nombre, se ha creado una cuenta para el correo $correo con la siguiente contraseña $contrasenaInvitado";
+        $cuerpo = "Estimado $nombre, se ha creado una cuenta para el correo $correo con la siguiente contraseña $contrasenaInvitado";
+        $cuerpo = "<!DOCTYPE html>
+        <html>
+        <head>
+        <title></title>
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <style type='text/css'>
+            @media screen {
+                @font-face {
+                  font-family: 'Lato';
+                  font-style: normal;
+                  font-weight: 400;
+                  src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');
+                }
+                
+                @font-face {
+                  font-family: 'Lato';
+                  font-style: normal;
+                  font-weight: 700;
+                  src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');
+                }
+                
+                @font-face {
+                  font-family: 'Lato';
+                  font-style: italic;
+                  font-weight: 400;
+                  src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');
+                }
+                
+                @font-face {
+                  font-family: 'Lato';
+                  font-style: italic;
+                  font-weight: 700;
+                  src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');
+                }
+            }
+            
+            
+            body, table, td, a { -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+            table, td { mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
+            img { -ms-interpolation-mode: bicubic; }
+        
+            
+            img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; }
+            table { border-collapse: collapse !important; }
+            body { height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important; }
+        
+           
+            a[x-apple-data-detectors] {
+                color: inherit !important;
+                text-decoration: none !important;
+                font-size: inherit !important;
+                font-family: inherit !important;
+                font-weight: inherit !important;
+                line-height: inherit !important;
+            }
+            
+            
+            @media screen and (max-width:600px){
+                h1 {
+                    font-size: 32px !important;
+                    line-height: 32px !important;
+                }
+            }
+        
+            
+            div[style*='margin: 16px 0;'] { margin: 0 !important; }
+        </style>
+        
+        </head>
+        <body style='background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;'>
+        
+        <!-- HIDDEN PREHEADER TEXT -->
+        <div style='display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;'>
+            We're thrilled to have you here! Get ready to dive into your new account.
+        </div>
+        
+        <table border='0' cellpadding='0' cellspacing='0' width='100%'>
+            <!-- LOGO -->
+            <tr>
+                <td bgcolor='#3A85A8' align='center'>
+                    
+                    <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                        <tr>
+                            <td align='center' valign='top' style='padding: 40px 10px 40px 10px;'>
+                                
+                            </td>
+                        </tr>
+                    </table>
+                    
+                </td>
+            </tr>
+            <!-- HERO -->
+            <tr>
+                <td bgcolor='#3A85A8' align='center' style='padding: 0px 10px 0px 10px;'>
+                    
+                    <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                        <tr>
+                            <td bgcolor='#ffffff' align='center' valign='top' style='padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;'>
+                              <h1 style='font-size: 48px; font-weight: 400; margin: 0;'>HOLA!</h1>
+                            </td>
+                        </tr>
+                    </table>
+                    
+                </td>
+            </tr>
+            <!-- COPY BLOCK -->
+            <tr>
+                <td bgcolor='#f4f4f4' align='center' style='padding: 0px 10px 0px 10px;'>
+                    
+                    <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                      <!-- COPY -->
+                      <tr>
+                        <td bgcolor='#ffffff' align='left' style='padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;' >
+                          <p style='margin: 0;'>".$cuerpo."</p><br>
+                          
+                        </td>
+                      </tr>
+                      
+                      <tr>
+                        <td bgcolor='#ffffff' align='left' style='padding: 0px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;' >
+                          <p style='margin: 0;'>Si tiene alguna duda, por favor consultar con el administrador Daniel Moreno .</p>
+                        </td>
+                      </tr>
+                      <!-- COPY -->
+                      <tr>
+                        <td bgcolor='#ffffff' align='left' style='padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;' >
+                          <p style='margin: 0;'>Daniel,<br>Comite Curricular,<br> Universidad de Talca, Curico</p>
+                        </td>
+                      </tr>
+                    </table>
+                    
+                </td>
+            </tr>
+            <!-- SUPPORT CALLOUT -->
+            <tr>
+                <td bgcolor='#f4f4f4' align='center' style='padding: 30px 10px 0px 10px;'>
+                    
+                    <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                        <!-- HEADLINE -->
+                        <tr>
+                          <td bgcolor='#FFECD1' align='center' style='padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;' >
+                            <h2 style='font-size: 20px; font-weight: 400; color: #111111; margin: 0;'>Utalca</h2>
+                            
+                          </td>
+                        </tr>
+                    </table>
+                    
+                </td>
+            </tr>
+            <!-- FOOTER -->
+            <tr>
+                <td bgcolor='#f4f4f4' align='center' style='padding: 0px 10px 0px 10px;'>
+                   
+                    <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                      <!-- NAVIGATION -->
+                      <tr>
+                        <td bgcolor='#f4f4f4' align='left' style='padding: 30px 30px 30px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;' >
+                          <p style='margin: 0;'>
+                            
+                          </p>
+                        </td>
+                      </tr>
+                      
+                    </table>
+                    
+                </td>
+            </tr>
+        </table>
+            
+        </body>
+        </html>";
+
         $mail->Body    = $cuerpo;
         
         include '../envio_de_correos/inferiorenviarcorreo.php';
@@ -61,12 +234,15 @@
         $consulta = "INSERT INTO relacionreunioninvitado (refcorreo, refid) VALUES ('$correo', '$idReunion')";
         echo $result=mysqli_query($conexion,$consulta);
         //Send Mail	
-
-        include '../envio_de_correos/superiorenviarcorreo.php';	
-        //Destinatario	
-        $mail->addAddress($correo, $nombre);   
-        //Contenido del correo
-        $mail->isHTML(true);
+        if($crear==false){
+            include '../envio_de_correos/superiorenviarcorreo.php';	
+            //Destinatario	
+            $mail->addAddress($correo, $nombre);
+            //Contenido del correo
+            $mail->isHTML(true);   
+        }
+        
+        
 
         $topico = "Invitacion a reunion $idReunion";                                  
         $mail->Subject = $topico;
@@ -94,7 +270,7 @@
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         <meta http-equiv='X-UA-Compatible' content='IE=edge' />
         <style type='text/css'>
-            /* FONTS */
+            
             @media screen {
                 @font-face {
                 font-family: 'Lato';
@@ -125,17 +301,17 @@
                 }
             }
             
-            /* CLIENT-SPECIFIC STYLES */
+            
             body, table, td, a { -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
             table, td { mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
             img { -ms-interpolation-mode: bicubic; }
 
-            /* RESET STYLES */
+            
             img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; }
             table { border-collapse: collapse !important; }
             body { height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important; }
 
-            /* iOS BLUE LINKS */
+            
             a[x-apple-data-detectors] {
                 color: inherit !important;
                 text-decoration: none !important;
@@ -145,7 +321,7 @@
                 line-height: inherit !important;
             }
             
-            /* MOBILE STYLES */
+            
             @media screen and (max-width:600px){
                 h1 {
                     font-size: 32px !important;
@@ -153,7 +329,7 @@
                 }
             }
 
-            /* ANDROID CENTER FIX */
+            
             div[style*='margin: 16px 0;'] { margin: 0 !important; }
         </style>
 
@@ -168,9 +344,7 @@
                     <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
                         <tr>
                             <td align='center' valign='top' style='padding: 40px 10px 40px 10px;'>
-                                <a href='http://litmus.com' target='_blank'>
-                                    <img alt='Logo' src='http://litmuswww.s3.amazonaws.com/community/template-gallery/ceej/logo.png' width='40' height='40' style='display: block; width: 40px; max-width: 40px; min-width: 40px; font-family: 'Lato', Helvetica, Arial, sans-serif; color: #ffffff; font-size: 18px;' border='0'>
-                                </a>
+                                
                             </td>
                         </tr>
                     </table>
--- generate_pdf.php
+++ generate_pdf.php
@@ -15,17 +15,93 @@ function selectReunion(){
     $tabla="";
    
                 while ($columna = mysqli_fetch_array( $resultado )){
-                $tabla.="
-                <h1>Comité Curricular ICC </h1>
-                <br>
-                <h2>Reunión ".$columna['tipoPredefinido']."</h2>
-                <h2>Fecha: ".$columna['fecha']."</h2>
-                <h2>Hora de Inicio:        ".$columna['hora']."</h2>
-                <h2>Duracion: ".$columna['duracion']." ".$columna['tipoDuracion']."</h2>
-                <br>
-                <h2>Temas Tratados: </h2>
-                <br>
-                <h2>".$columna['nombre']."</h2>
+                $tabla="
+                
+                <body style='background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;'>
+                    <table border='0' cellpadding='0' cellspacing='0' width='100%'>
+                    <!-- LOGO -->
+                    <tr>
+                        <td bgcolor='#B916CD' align='center'>
+                            
+                            <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                                <tr>
+                                    <td align='center' valign='top' style='padding: 40px 10px 40px 10px;'>
+                                         
+                                    </td>
+                                </tr>
+                            </table>
+                            
+                        </td>
+                    </tr>
+                    <!-- HERO -->
+                    <tr>
+                        <td bgcolor='#B916CD' align='center' style='padding: 0px 10px 0px 10px;'>
+                            
+                            <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                                <tr>
+                                    <td bgcolor='#ffffff' align='center' valign='top' style='padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;'>
+                                    <h1 style='font-size: 48px; font-weight: 400; margin: 0;'>Resumen!</h1>
+                                    </td>
+                                </tr>
+                            </table>
+                            
+                        </td>
+                    </tr>
+                    <!-- COPY BLOCK -->
+                    <tr>
+                        <td bgcolor='#f4f4f4' align='center' style='padding: 0px 10px 0px 10px;'>
+                            
+                            <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                            <!-- COPY -->
+                            <tr>
+                                <td bgcolor='#ffffff' align='left' style='padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;' >
+                                
+                                <h2>Reunión ".$columna['tipoPredefinido']."</h2>
+                                <h2>Fecha: ".$columna['fecha']."</h2>
+                                <h2>Hora de Inicio:        ".$columna['hora']."</h2>
+                                <h2>Duracion: ".$columna['duracion']." ".$columna['tipoDuracion']."</h2>
+                                <br>
+                                <h2>Temas Tratados: </h2>
+                                <br>
+                                <h2>".$columna['nombre']."</h2>
+                                    
+                                </td>
+                            </tr>
+                            <!-- BULLETPROOF BUTTON -->
+                            
+                            
+                            
+                    
+                            <!-- COPY -->
+                            <tr>
+                                <td bgcolor='#ffffff' align='left' style='padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;' >
+                                <p style='margin: 0;'>Daniel,<br>Comite Curricular<br> Utalca, Ing. Civil Computacion </p>
+                                </td>
+                            </tr>
+                            </table>
+                            
+                        </td>
+                    </tr>
+                    <!-- SUPPORT CALLOUT -->
+                    <tr>
+                        <td bgcolor='#f4f4f4' align='center' style='padding: 30px 10px 0px 10px;'>
+                            
+                            <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;' >
+                                <!-- HEADLINE -->
+                                <tr>
+                                <td bgcolor='#FFECD1' align='center' style='padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;' >
+                                    <h2 style='font-size: 20px; font-weight: 400; color: #111111; margin: 0;'></h2>
+                                    
+                                </td>
+                                </tr>
+                            </table>
+                            
+                        </td>
+                    </tr>
+                    
+                    </table>
+                    
+                </body>
             ";
                    
         } 
